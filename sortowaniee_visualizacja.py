12a9b07d24e4bf12c0421102f903c1012c890e27
acadiuss <84239509+acadiuss@users.noreply.github.com>
2023-10-16 00:51:49 +0200

    Update sortowaniee_visualizacja.py

--- sortowaniee_visualizacja.py
+++ sortowaniee_visualizacja.py
@@ -1,4 +1,4 @@
-
+import sys
 import matplotlib.pyplot as plt
 import numpy as np
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QComboBox, QVBoxLayout, QWidget
@@ -17,6 +17,7 @@ class SortingVisualizer(QMainWindow):
         self.central_widget = QWidget(self)
         self.setCentralWidget(self.central_widget)
         self.layout = QVBoxLayout(self.central_widget)
+       
 
         self.canvas = PlotCanvas(self, width=5, height=4)
         self.layout.addWidget(self.canvas)
@@ -45,119 +46,17 @@ class SortingVisualizer(QMainWindow):
 
     def sorting_worker(self, selected_algorithm):
         if selected_algorithm == 'Bubble Sort':
-            self.bubble_sort()
+            sorter = BubbleSort(self.data.copy(), self.update_canvas)
         elif selected_algorithm == 'Insertion Sort':
-            self.insertion_sort()
+            sorter = InsertionSort(self.data.copy(), self.update_canvas)
         elif selected_algorithm == 'Selection Sort':
-            self.selection_sort()
+            sorter = SelectionSort(self.data.copy(), self.update_canvas)
         elif selected_algorithm == 'Quick Sort':
-            self.quick_sort(self.data, 0, len(self.data) - 1)
+            sorter = QuickSort(self.data.copy(), self.update_canvas)
         elif selected_algorithm == 'Merge Sort':
-            self.data #= self.merge_sort(self.data)
-
-    def bubble_sort(self):
-        data = self.data.copy()
-        for i in range(len(data)):
-            for j in range(0, len(data) - i - 1):
-                if data[j] > data[j + 1]:
-                    data[j], data[j + 1] = data[j + 1], data[j]
-                self.update_canvas(data)
-                time.sleep(0.1)
-
-    def insertion_sort(self):
-        data = self.data.copy()
-        for i in range(1, len(data)):
-            key = data[i]
-            j = i - 1
-            while j >= 0 and key < data[j]:
-                data[j + 1] = data[j]
-                j -= 1
-            data[j + 1] = key
-            self.update_canvas(data)
-            time.sleep(0.1)
-
-    def selection_sort(self):
-        data = self.data.copy()
-        for i in range(len(data)):
-            min_idx = i
-            for j in range(i + 1, len(data)):
-                if data[j] < data[min_idx]:
-                    min_idx = j
-            data[i], data[min_idx] = data[min_idx], data[i]
-            self.update_canvas(data)
-            time.sleep(0.1)
-
-    def partition(self, arr, low, high):
-        i = low - 1
-        pivot = arr[high]
-        for j in range(low, high):
-            if arr[j] < pivot:
-                i += 1
-                arr[i], arr[j] = arr[j], arr[i]
-        arr[i + 1], arr[high] = arr[high], arr[i + 1]
-        return i + 1
-
-    def quick_sort(self, arr, low, high):
-        if low < high:
-            pi = self.partition(arr, low, high)
-            self.quick_sort(arr, low, pi - 1)
-            self.quick_sort(arr, pi + 1, high)
-            self.update_canvas(arr)
-
-    def merge(self, arr, left, mid, right):
-        n1 = mid - left + 1
-        n2 = right - mid
-        L = [0] * n1
-        R = [0] * n2
-        for i in range(n1):
-            L[i] = arr[left + i]
-        for j in range(n2):
-            R[j] = arr[mid + 1 + j]
-        i = j = 0
-        k = left
-        while i < n1 and j < n2:
-            if L[i] <= R[j]:
-                arr[k] = L[i]
-                i += 1
-            else:
-                arr[k] = R[j]
-                j += 1
-            k += 1
-        while i < n1:
-            arr[k] = L[i]
-            i += 1
-            k += 1
-        while j < n2:
-            arr[k] = R[j]
-            j += 1
-            k += 1
-
-    def merge_sort(self, arr):
-        if len(arr) > 1:
-            mid = len(arr) // 2
-            L = arr[:mid]
-            R = arr[mid:]
-            self.merge_sort(L)
-            self.merge_sort(R)
-            i = j = k = 0
-            while i < len(L) and j < len(R):
-                if L[i] < R[j]:
-                    arr[k] = L[i]
-                    i += 1
-                else:
-                    arr[k] = R[j]
-                    j += 1
-                k += 1
-            while i < len(L):
-                arr[k] = L[i]
-                i += 1
-                k += 1
-            while j < len(R):
-                arr[k] = R[j]
-                j += 1
-                k += 1
-            self.update_canvas(arr)
-        return arr
+            sorter = MergeSort(self.data.copy(), self.update_canvas)
+        
+        sorter.sort()
 
     def update_canvas(self, data):
         self.canvas.clear()
@@ -169,15 +68,122 @@ class PlotCanvas(FigureCanvas):
         self.fig = Figure(figsize=(width, height), dpi=dpi)
         self.ax = self.fig.add_subplot(111)
         super().__init__(self.fig)
-        
+
     def bar(self, x, y, **kwargs):
         self.ax.bar(x, y, **kwargs)
-        
+
     def clear(self):
         self.ax.clear()
 
+class SortingAlgorithm:
+    def __init__(self, data, callback):
+        self.data = data
+        self.callback = callback
+
+    def sort(self):
+        raise NotImplementedError("Subclasses must implement the 'sort' method.")
+
+class BubbleSort(SortingAlgorithm):
+    def sort(self):
+        data = self.data
+        for i in range(len(data)):
+            for j in range(0, len(data) - i - 1):
+                if data[j] > data[j + 1]:
+                    data[j], data[j + 1] = data[j + 1], data[j]
+                self.callback(data)
+                time.sleep(0.1)
+
+class InsertionSort(SortingAlgorithm):
+    def sort(self):
+        data = self.data
+        for i in range(1, len(data)):
+            key = data[i]
+            j = i - 1
+            while j >= 0 and key < data[j]:
+                data[j + 1] = data[j]
+                j -= 1
+            data[j + 1] = key
+            self.callback(data)
+            time.sleep(0.1)
+
+class SelectionSort(SortingAlgorithm):
+    def sort(self):
+        data = self.data
+        for i in range(len(data)):
+            min_idx = i
+            for j in range(i + 1, len(data)):
+                if data[j] < data[min_idx]:
+                    min_idx = j
+            data[i], data[min_idx] = data[min_idx], data[i]
+            self.callback(data)
+            time.sleep(0.1)
+
+class QuickSort(SortingAlgorithm):
+    def partition(self, low, high):
+        i = low - 1
+        pivot = self.data[high]
+        for j in range(low, high):
+            if self.data[j] < pivot:
+                i += 1
+                self.data[i], self.data[j] = self.data[j], self.data[i]
+                self.data[i + 1], self.data[high] = self.data[high], self.data[i + 1]
+        return i + 1
+
+    def sort(self):
+        self.quick_sort(0, len(self.data) - 1)
+
+    def quick_sort(self, low, high):
+        if low < high:
+            pi = self.partition(low, high)
+            self.quick_sort(low, pi - 1)
+            self.quick_sort(pi + 1, high)
+            self.callback(self.data)
+            time.sleep(0.1)
+
+class MergeSort(SortingAlgorithm):
+    def merge(self, left, mid, right):
+        n1 = mid - left + 1
+        n2 = right - mid
+        L = [0] * n1
+        R = [0] * n2
+        for i in range(n1):
+            L[i] = self.data[left + i]
+        for j in range(n2):
+            R[j] = self.data[mid + 1 + j]
+        i = j = 0
+        k = left
+        while i < n1 and j < n2:
+            if L[i] <= R[j]:
+                self.data[k] = L[i]
+                i += 1
+            else:
+                self.data[k] = R[j]
+                j += 1
+            k += 1
+        while i < n1:
+            self.data[k] = L[i]
+            i += 1
+            k += 1
+        while j < n2:
+            self.data[k] = R[j]
+            j += 1
+            k += 1
+        self.callback(self.data)
+
+    def merge_sort(self, left, right):
+        if left < right:
+            mid = (left + right) // 2
+            self.merge_sort(left, mid)
+            self.merge_sort(mid + 1, right)
+            self.merge(left, mid, right)
+            self.callback(self.data)
+
+    def sort(self):
+        self.merge_sort(0, len(self.data) - 1)
+
 if __name__ == '__main__':
-    app = QApplication([])
+    app = QApplication(sys.argv)
     window = SortingVisualizer()
     window.show()
     app.exec()
+
